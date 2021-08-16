edec2beead8abe5208627a3116e9b7416ca17c24
Renier Trenuela <rtrenuela@objectbright.com>
2021-08-16 16:27:57 +0800

    password hash

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -54,7 +54,7 @@ class UsersController extends Controller
 
     public function store(UsersStoreRequest $request)
     {
-        User::create(["name" => $request->name, "email" => $request->email, "password" => $request->password,]);
+        User::create(["name" => $request->name, "email" => $request->email, "password" => Hash::make($request->password),]);
 
         return ['success' => true];
     }
